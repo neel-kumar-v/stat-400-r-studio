931c48a15a38403166f1339733fe1dfb09c263c9
neel-kumar-v <greenworldwins@gmail.com>
2025-03-04 23:32:08 -0600

    Update fair-die.r

--- fair-die.r
+++ fair-die.r
@@ -6,9 +6,10 @@ sum <- die1 + die2
 prob1 <- length(sum[sum <= 6]) / n
 prob1 
 
-prob2 <- length(sum[sum <= 6 & die1 > 2]) / count
+prob2 <- length(sum[sum <= 6 & die1 > 2]) / length(sum[sum <= 6])
 prob2
 
 # Error compared to theoretical value
 abs(prob1 - 15/36) / (15/36)
 abs(prob2 - 0.4) / (0.4)
+
